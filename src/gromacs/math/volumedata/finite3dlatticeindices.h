45b3c368ba403766593c81825c29d0bb75cbc5d0
Christian Blau <cblau@gwdg.de>
2017-10-09 10:47:55 +0200

    Moving finite3dLattice content into header, remove ununsed routines.
    Change-Id: Ib7f8d579974edbf3047936b2fe93f07ad815e63a

--- src/gromacs/math/volumedata/finite3dlatticeindices.h
+++ src/gromacs/math/volumedata/finite3dlatticeindices.h
@@ -2,8 +2,10 @@
 #define GMX_MATH_FINITE3DLATTICEINDICES
 
 #include "gromacs/math/vectypes.h"
+#include "gromacs/utility/exceptions.h"
 
 #include <vector>
+#include <string>
 
 namespace gmx
 {
@@ -12,9 +14,13 @@ class Finite3DLatticeIndices
 {
     public:
         Finite3DLatticeIndices() = default;
-        Finite3DLatticeIndices(const std::vector<int> &extend);
-        int getNumLatticePoints() const;           //!< returns extend[0]*extend[1]*extend[2]
-        const std::vector<int> &getExtend() const; //!< return the extend of the lattice
+        Finite3DLatticeIndices(const std::vector<int> &extend){    setExtend(extend); };
+
+        int getNumLatticePoints() const
+        {
+            return extend_[XX] *extend_[YY] * extend_[ZZ];
+        };                                                            //!< returns extend[0]*extend[1]*extend[2]
+        const std::vector<int> &getExtend() const{ return extend_; }; //!< return the extend of the lattice
 
         /*! \brief
          * The extend of the lattice.
@@ -22,33 +28,66 @@ class Finite3DLatticeIndices
          * Lattice indices will alwasy run from (0,0,0) to
          * (extend[XX]-1,extend[YY]-1,extend[ZZ]-1)
          */
-        void setExtend(const std::vector<int> &extend);
+        void setExtend(const std::vector<int> &extend)
+        {
+            if (!allNonNegative(extend))
+            {
+                GMX_THROW(RangeError("Lattice extend must be larger or equal zero in all dimensions but is "+std::to_string(extend[XX]) + " , " + std::to_string(extend[YY])+ " , " + std::to_string(extend[ZZ]) + " ."));
+            }
+            extend_          = extend;
+        };
 
         /*! \brief
          * Unique one-dimensional lattice index.
          *
          * x + extend_x * y + extend_x * extend_y * z.
          */
-        std::size_t getLinearIndexFromLatticeIndex(const std::vector<int> &latticeIndex) const;
+        std::size_t getLinearIndexFromLatticeIndex(const std::vector<int> &latticeIndex) const
+        {
+            auto result = latticeIndex[XX] > -1 ? latticeIndex[XX] : extend_[XX] + latticeIndex[XX];
+            result += latticeIndex[YY] > -1 ? extend_[XX] * latticeIndex[YY]
+                : extend_[XX] * (extend_[YY] + latticeIndex[YY]);
+            result += latticeIndex[ZZ] > -1 ? extend_[XX] *extend_[YY] * latticeIndex[ZZ]
+                : extend_[XX] *extend_[YY] * (extend_[ZZ] + latticeIndex[ZZ]);
+            return result;
+        }
 
         /*! \brief
          * Inverse of getLinearIndexFromLatticeIndex
          */
-        std::vector<int> getLatticeIndexFromLinearIndex(int linearIndex) const;
+        std::vector<int> getLatticeIndexFromLinearIndex(int linearIndex) const
+        {
+            std::vector<int> result;
+            result[XX] = (linearIndex % extend_[XX]) % extend_[YY];
+            result[YY] = (linearIndex / extend_[XX]) % extend_[YY];
+            result[ZZ] = (linearIndex / extend_[XX]) / extend_[YY];
+            return result;
+        }
 
         /*! \brief
          * True if latticeIndex is in Lattice
          */
-        bool inLattice(const std::vector<int> &latticeIndex) const;
+        bool inLattice(const std::vector<int> &latticeIndex) const
+        {
 
-        /*! \brief
-         * True if latticeIndex is in index along given dimension
-         */
-        bool inLattice(int latticeIndex, int dimension) const;
+            if (!allNonNegative(latticeIndex))
+            {
+                return false;
+            }
+            if ((latticeIndex[XX] >= extend_[XX]) || (latticeIndex[YY] >= extend_[YY]) ||
+                (latticeIndex[ZZ] >= extend_[ZZ]))
+            {
+                return false;
+            }
+            return true;
+        }
         /*! \brief
          * Is true if all
          */
-        bool allNonNegative(const std::vector<int> &extend) const;
+        bool allNonNegative(const std::vector<int> &extend) const
+        {
+            return (extend[XX] >= 0) && (extend[YY] >= 0) && (extend[ZZ] >= 0);
+        }
     private:
         std::vector<int>  extend_;
 };
